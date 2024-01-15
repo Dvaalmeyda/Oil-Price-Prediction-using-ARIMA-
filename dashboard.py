86cd133063062aec702dc3d38034717f51304f3b
dvaalmeyda <divaalmeida99@gmail.com>
2024-01-15 11:27:15 +0700

    perbaikan 2

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,8 @@ Email: diva.putra.almeyda@students.untidar.ac.id
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 import plotly.subplots as sp
 import plotly.graph_objects as go
